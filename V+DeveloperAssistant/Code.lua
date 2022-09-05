20fbe8bd3451af68b00d8ba4983ea959959bd1ce
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2022-09-05 22:33:37 +0300

    Update Code.lua

--- V+DeveloperAssistant/Code.lua
+++ V+DeveloperAssistant/Code.lua
@@ -1,22 +1,22 @@
-local i = ''
+local class = ''
 local f = CreateFrame('FRAME')
 f:RegisterEvent('PLAYER_LOGIN')
 f:SetScript('OnEvent', function()
-	i = UnitClass('player')
-	if not VP_DA_TALENTS    then VP_DA_TALENTS    = {} end
-	if not VP_DA_TALENTS[i] then VP_DA_TALENTS[i] = {} end
+	class = UnitClass('player')
+	if not VP_DA_TALENTS then VP_DA_TALENTS = {} end
+	if not VP_DA_TALENTS[class] then VP_DA_TALENTS[i] = {} end
 end)
 
 local HookSetTalent = GameTooltip.SetTalent
 function GameTooltip.SetTalent(self, tabIndex, talentIndex)
 	HookSetTalent(self, tabIndex, talentIndex)
 	if IsAltKeyDown() then
-		local j = tabIndex .. '-' .. talentIndex
-		VP_DA_TALENTS[i][j] = {}
+		local talent = tabIndex .. '-' .. talentIndex
+		VP_DA_TALENTS[class][talent] = {}
 		for i = 1, GameTooltip:NumLines() do
-			table.insert(VP_DA_TALENTS[i][j], getglobal('GameTooltipTextLeft' .. i):GetText())
+			table.insert(VP_DA_TALENTS[class][talent], getglobal('GameTooltipTextLeft' .. i):GetText())
 			if getglobal('GameTooltipTextRight' .. i):IsVisible() then
-				table.insert(VP_DA_TALENTS[i][j], getglobal('GameTooltipTextRight' .. i):GetText())
+				table.insert(VP_DA_TALENTS[class][talent], getglobal('GameTooltipTextRight' .. i):GetText())
 			end
 		end
 	end

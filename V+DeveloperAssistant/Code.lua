a1f3a5035d851f56b31e8251960984143006462b
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2022-09-05 23:07:57 +0300

    Update Code.lua

--- V+DeveloperAssistant/Code.lua
+++ V+DeveloperAssistant/Code.lua
@@ -1,3 +1,4 @@
+local _G = getfenv(0)
 local class = ''
 local f = CreateFrame('FRAME')
 f:RegisterEvent('PLAYER_LOGIN')
@@ -22,7 +23,7 @@ function GameTooltip.SetTalent(self, tabIndex, talentIndex)
 	end
 end
 ]]
-
+--[[
 local HookSetTalent = GameTooltip.SetTalent
 function GameTooltip.SetTalent(self, tabIndex, talentIndex)
 	HookSetTalent(self, tabIndex, talentIndex)
@@ -38,3 +39,40 @@ function GameTooltip.SetTalent(self, tabIndex, talentIndex)
 		table.insert(VP_DA_TALENTS[class], tableTalent)
 	end
 end
+]]
+
+
+local HookSetTalent = GameTooltip.SetTalent
+function GameTooltip.SetTalent(self, tabIndex, talentIndex)
+	HookSetTalent(self, tabIndex, talentIndex)
+	local tableTalent = {}
+	table.insert(tableTalent, tabIndex .. '-' .. talentIndex)
+	for i = 1, GameTooltip:NumLines() do
+		table.insert(tableTalent, getglobal('GameTooltipTextLeft' .. i):GetText())
+		if getglobal('GameTooltipTextRight' .. i):IsVisible() then
+			table.insert(tableTalent, getglobal('GameTooltipTextRight' .. i):GetText())
+		end
+	end
+	table.insert(VP_DA_TALENTS[class], tableTalent)
+end
+
+SLASH_DA_GET_TALENTS1 = '/gt'
+function SlashCmdList.DA_GET_TALENTS(msg)
+	DEFAULT_CHAT_FRAME:AddMessage('Get talents - Start')
+	for i = 1, GetNumTalentTabs() do
+		for j = 1, GetNumTalents(i) do
+			GameTooltip:SetOwner(UIParent)
+			GameTooltip:ClearLines()
+			GameTooltip:SetTalent(i, j)
+			GameTooltip:Hide()
+		end
+	end
+	DEFAULT_CHAT_FRAME:AddMessage('Get talents - Stop')
+end
+
+
+
+
+
+
+
